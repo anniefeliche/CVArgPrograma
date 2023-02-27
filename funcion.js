050ad0fca6791b1063146e96fc462cc19f80ca97
anniefeliche <125110779+anniefeliche@users.noreply.github.com>
2023-02-27 18:28:18 -0300

    Add files via upload

---
+++ funcion.js
@@ -0,0 +1,23 @@
+function getlink()
+{
+    var insta = document.getElementById("link1");
+    //document.getElementById("resultado1").innerHTML= insta.href;
+}
+
+function getlink()
+{
+    var face = document.getElementById("link2");
+}
+
+function getlink()
+{
+    var twit = document.getElementById("link3");
+    //document.getElementById("resultado3").innerHTML= twit.href;
+}
+
+function getlink()
+{
+    var linke = document.getElementById("link4");
+}
+    
+
